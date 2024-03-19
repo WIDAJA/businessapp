1b2c91564bdb51b2e51b66e0d4b626deecc0f10b
Danielsm <eduardodaniel797@gmail.com>
2024-03-19 18:43:26 -0500

    Responsive home y nuevo componente de servicios para mobile

---
+++ src/components/Preact/Service.jsx
@@ -0,0 +1,30 @@
+import { Component } from "preact";
+
+export default class Service extends Component {
+
+  render({ menu }, { }) {
+
+    return (
+      <>
+        <h1 class="sm:hidden text-4xl text-center">- Servicios -</h1>
+        <h2 class="text-center">
+          Estos son algunos de los servicios que tenemos para ayudar a impulsar tu
+          negocio
+        </h2>
+        {
+          menu.map((x, i) => {
+
+            const className = `mt-10 border-2 py-7 px-4 rounded-xl ${!(i % 2) ? 'border-accent' : 'border-green-400'} shadow-lg shadow-secondary`
+
+            return (
+              <section className={className}>
+                <header class="text-center text-2xl font-bold">{x.name}</header>
+                <p class="mt-4">{x.text}</p>
+              </section>
+            )
+          })
+        }
+      </>
+    )
+  }
+}
