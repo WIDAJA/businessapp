15302846abf5944d7d5671252a81860868a83a37
Danielsm <eduardodaniel797@gmail.com>
2024-03-26 08:25:29 -0500

    Eliminación de la url del sitio

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,6 @@ import vercel from "@astrojs/vercel/serverless";
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-  site: 'mybusinesapp.com',
   integrations: [tailwind(), db(), preact({
     compat: true
   })],
